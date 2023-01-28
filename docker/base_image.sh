80aea8dc5bcc9a84ae9df10b52264c0599a84345
wenjie.yin <wenjie.yin@aispeech.com>
2023-01-28 13:43:59 +0800

    feat: add debug params

--- docker/base_image.sh
+++ docker/base_image.sh
@@ -1,1 +1,4 @@
 docker build -t server:base -f Dockerfile_base .
+
+# docker run
+# docker run -ti --privileged --rm -v /home/wenjie.yin/github/simplest-web-server:/opt/simplest-web-server server:base bash
